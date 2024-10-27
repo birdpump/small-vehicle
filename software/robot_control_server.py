d2772ef3fb89185140611dcd1ff2d402b909e092
nitsirc0 <83107916+nitsirc0@users.noreply.github.com>
2024-10-26 18:44:31 -0700

    Update robot_control_server.py

--- software/robot_control_server.py
+++ software/robot_control_server.py
@@ -180,22 +180,22 @@ def update_angle():
 
         #print(pan1, '-', tilt1)
 
-        if tilt1 < -0.05 and tilt_angle >= -1:
-            tilt_angle -= abs(tilt1)/5
-        elif tilt1 > 0.05  and tilt_angle <= 1:
-            tilt_angle += abs(tilt1)/5
+        # if tilt1 < -0.05 and tilt_angle >= -1:
+        #     tilt_angle -= abs(tilt1)/5
+        # elif tilt1 > 0.05  and tilt_angle <= 1:
+        #     tilt_angle += abs(tilt1)/5
         
-        if pan1 < -0.05 and pan_angle >= -1:
-            pan_angle -= abs(pan1)/5
-        elif pan1 > 0.05  and pan_angle <= 1:
-            pan_angle += abs(pan1)/5
+        # if pan1 < -0.05 and pan_angle >= -1:
+        #     pan_angle -= abs(pan1)/5
+        # elif pan1 > 0.05  and pan_angle <= 1:
+        #     pan_angle += abs(pan1)/5
         
         # if pan1 > -0.3 and pan1 < 0.3:
         #     pan1 = 0
 
         # Clamp the input value to the range [-1, 1]
-        pane = max(-1, min(1, -pan_angle))
-        tilte = max(-0.5, min(0.51, tilt_angle))
+        pane = max(-1, min(1, -pan1))
+        tilte = max(-0.5, min(0.51, tilt1))
         # Calculate the current angle based on the value
 
         angle1 = convert_to_angle(pane)
